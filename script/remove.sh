d9b0f6a347580a5a1687eb459871f24e65125e48
chengshiwen <chengshiwen0103@gmail.com>
2020-12-04 14:53:47 +0800

    update scripts

---
+++ script/remove.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker rm -f -v influxdb-1 influxdb-2 influxdb-3 influxdb-4
--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-docker run -d --name influxdb-1 -p 8086:8086 influxdb:1.7
-docker run -d --name influxdb-2 -p 8087:8086 influxdb:1.7
-docker run -d --name influxdb-3 -p 8088:8086 influxdb:1.7
-docker run -d --name influxdb-4 -p 8089:8086 influxdb:1.7
+if [[ $# -gt 1 ]] || [[ "$1" != "" && "$1" != "auth" && "$1" != "https" ]]; then
+    echo "Usage: $0 [auth|https]"
+    exit 1
+fi
 
+if [[ "$1" == "auth" ]]; then
+    echo "Mode: $1"
+    OPTIONS="-e INFLUXDB_HTTP_AUTH_ENABLED=true -e INFLUXDB_ADMIN_USER=admin -e INFLUXDB_ADMIN_PASSWORD=admin"
+elif [[ "$1" == "https" ]]; then
+    echo "Mode: $1"
+    OPTIONS="-v ${PWD}/cert:/cert -e INFLUXDB_HTTP_HTTPS_ENABLED=true -e INFLUXDB_HTTP_HTTPS_CERTIFICATE=/cert/tls.crt -e INFLUXDB_HTTP_HTTPS_PRIVATE_KEY=/cert/tls.key"
+else
+    echo "Mode: default"
+fi
 
-# docker run -d --name influxdb-1 -p 8086:8086 -v ${PWD}/cert:/cert -e INFLUXDB_HTTP_HTTPS_ENABLED=true -e INFLUXDB_HTTP_HTTPS_CERTIFICATE=/cert/tls.crt -e INFLUXDB_HTTP_HTTPS_PRIVATE_KEY=/cert/tls.key influxdb:1.7
-# docker run -d --name influxdb-2 -p 8087:8086 -v ${PWD}/cert:/cert -e INFLUXDB_HTTP_HTTPS_ENABLED=true -e INFLUXDB_HTTP_HTTPS_CERTIFICATE=/cert/tls.crt -e INFLUXDB_HTTP_HTTPS_PRIVATE_KEY=/cert/tls.key influxdb:1.7
-# docker run -d --name influxdb-3 -p 8088:8086 -v ${PWD}/cert:/cert -e INFLUXDB_HTTP_HTTPS_ENABLED=true -e INFLUXDB_HTTP_HTTPS_CERTIFICATE=/cert/tls.crt -e INFLUXDB_HTTP_HTTPS_PRIVATE_KEY=/cert/tls.key influxdb:1.7
-# docker run -d --name influxdb-4 -p 8089:8086 -v ${PWD}/cert:/cert -e INFLUXDB_HTTP_HTTPS_ENABLED=true -e INFLUXDB_HTTP_HTTPS_CERTIFICATE=/cert/tls.crt -e INFLUXDB_HTTP_HTTPS_PRIVATE_KEY=/cert/tls.key influxdb:1.7
+docker run -d --name influxdb-1 -p 8086:8086 ${OPTIONS} influxdb:1.8
+docker run -d --name influxdb-2 -p 8087:8086 ${OPTIONS} influxdb:1.8
+docker run -d --name influxdb-3 -p 8088:8086 ${OPTIONS} influxdb:1.8
+docker run -d --name influxdb-4 -p 8089:8086 ${OPTIONS} influxdb:1.8
